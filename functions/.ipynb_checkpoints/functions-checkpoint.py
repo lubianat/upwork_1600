48c3847393b14f7b789ef743ccf134e7a18f1be3
lubianat <tiago.lubiana.alves@usp.br>
2020-10-20 17:56:49 -0300

    chore: Reformat and re-run code for people

--- functions/.ipynb_checkpoints/functions-checkpoint.py
+++ functions/.ipynb_checkpoints/functions-checkpoint.py
@@ -5,6 +5,119 @@ import time
 import os
 import wikitextparser as wtp
 import mwparserfromhell
+from tqdm import tqdm
+
+def detect_and_save_entries_per_section(page):
+    
+    sections_df = get_sections_dataframe(page)
+    
+    entry_infos_by_section = {}
+    for i in range(0,len(sections_df)):
+        print(i+1)
+        try:
+            entry_info = get_bullet_info_for_section(page, n):
+        except:
+            entry_info = "failed"
+
+        entry_infos_by_section[sections_df["anchor"][i]] =  full_path
+
+    full_path = "./"+ page   + "/Entries"
+    
+    my_makedir("./"+ page)
+    my_makedir(full_path)
+
+    for i in entry_infos_by_section:
+        print(i)
+        title = full_path + "/" + i + ".csv"
+        try:
+            (entry_infos_by_section[i].to_csv(title))
+        except:
+            pass  
+
+def get_bullet_info_for_section(page, section):
+
+    df = get_bullets_on_page_section(page, section)
+    df["id"] = ["entry_" + str(a+ 1)for a in df.index ]
+    df = get_years_from_bullets(df)
+    df = get_main_text(df)
+    df = get_main_info(df)
+    
+    page_titles = df["main_link_text"].values
+    title_to_page_id = get_wikipedia_page_ids(page_titles)
+    df["main_link_id"] = df["main_link_text"].map(title_to_page_id)
+    df = get_other_link_info(df)
+    
+    return(df)
+
+
+def get_other_link_info(df):
+
+    other_link_texts = {}
+    other_link_urls = {}
+    other_link_ids = {}
+
+    for i in tqdm(range(len(df))):
+        text = df["wikitext_string"][i]
+        entry = df["id"][i]
+        wikilinks = wtp.parse(text).wikilinks
+
+        target = wikilinks[0].target
+
+        if len(wikilinks) > 1:
+            wikilinks.pop(0)
+
+            targets = [link.target for link in wikilinks]
+            other_link_texts[entry]= " ; ".join(targets)
+
+            end_of_urls = [target.replace(" ", "_") for target in targets]
+            target_urls = ["https://en.wikipedia.org/wiki/" + end_of_url for end_of_url in end_of_urls]
+            other_link_urls[entry] = " ; ".join(target_urls)
+
+            title_to_page_id = get_wikipedia_page_ids(targets)
+
+            try:
+                target_ids = [title_to_page_id[x] for x in targets]
+            except:
+                target_ids = list(title_to_page_id.values())
+
+            other_link_ids[entry] = " ; ".join(target_ids)
+
+        else:
+            other_link_texts[entry] = None
+            other_link_urls[entry] = None
+            other_link_ids[entry] = None
+        
+        time.sleep(1)
+
+
+    df["other_link_text"] = df["id"].map(other_link_texts)
+    df["other_link_url"] = df["id"].map(other_link_urls)
+    df["other_link_id"] = df["id"].map(other_link_ids)
+    
+    return(df)
+
+
+def get_main_info(df):
+
+    main_link_texts = {}
+    main_link_urls = {}
+
+    for i, row in df.iterrows():
+        text = row["wikitext_string"]
+        entry = row["id"]
+        wikilinks = wtp.parse(text).wikilinks
+
+        target = wikilinks[0].target
+
+        main_link_texts[entry] = target
+
+        end_of_url =  target.replace(" ", "_")
+        main_link_urls[entry] = "https://en.wikipedia.org/wiki/" + end_of_url
+        
+    df["main_link_text"] = df["id"].map(main_link_texts)
+    df["main_link_urls"] = df["id"].map(main_link_urls)
+    
+    return(df)
 
 def get_main_text(df):
     full_texts = {}
@@ -74,6 +187,14 @@ def get_sections_dataframe(page_title):
     return(sections_df)
 
 
+def my_makedir(path):
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
 def detect_and_save_people_per_section(page):
     query = "https://en.wikipedia.org/w/api.php?action=parse&prop=sections&page="+page+"&format=json"
     sections = requests.get(query)
@@ -85,30 +206,26 @@ def detect_and_save_people_per_section(page):
         print(i+1)
         n = i+1
         try:
-            human_info = functions.get_human_info_for_section(page, n)
+            human_info = get_human_info_for_section(page, n)
         except:
             try:
-                human_info = functions.get_human_info_for_section(page, n)
+                human_info = get_human_info_for_section(page, n)
             except:
                 try:
-                    human_info = functions.get_human_info_for_section(page, n)
+                    human_info = get_human_info_for_section(page, n)
                 except:
                     human_info = "failed"
 
         human_infos_by_section[sections_df["anchor"][i]] =  human_info
 
-    path = "./"+page
-
-    try:
-        os.mkdir(path)
-    except OSError:
-        print ("Creation of the directory %s failed" % path)
-    else:
-        print ("Successfully created the directory %s " % path)
+    full_path = "./"+ page   + "/People"
+    
+    my_makedir("./"+ page)
+    my_makedir(full_path)
 
     for i in human_infos_by_section:
         print(i)
-        title = "./"+page + "/" + i + ".csv"
+        title = full_path + "/" + i + ".csv"
         try:
             (human_infos_by_section[i].to_csv(title))
         except:
@@ -216,3 +333,4 @@ def get_wikipedia_page_ids(page_titles):
     title_to_page_id  = {
         page_info['title']: page_id
         for page_id, page_info in json_response['query']['pages'].items()}
+    return(title_to_page_id)
